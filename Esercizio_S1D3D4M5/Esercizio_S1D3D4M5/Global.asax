79f42bbc3b5f9c6cae44f5c2ca6392b6cc2a6757
Aucons94 <148054502+Aucons94@users.noreply.github.com>
2024-02-29 16:46:14 +0100

    inserimento

---
+++ Esercizio_S1D3D4M5/Esercizio_S1D3D4M5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Esercizio_S1D3D4M5.MvcApplication" Language="C#" %>
---
+++ Esercizio_S1D3D4M5/Esercizio_S1D3D4M5/Models/Articolo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Esercizio_S1D3D4M5.Models
+{
+    public class Articolo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Prezzo { get; set; }
+        public string Descrizione { get; set; }
+        public string ImmagineCopertinaUrl { get; set; }
+        public string ImmaginiAggiuntiveUrls1 { get; set; }
+        public string ImmaginiAggiuntiveUrls2 { get; set; }
+        public bool StatoDisponibile { get; set; }
+    }
+}
